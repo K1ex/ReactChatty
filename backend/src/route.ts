ab34abea9e98d8beeb6ed1d72655d0bff5f8526a
zhaoyikai <bl-user1002@hotmail.com>
2022-11-09 21:46:47 +0800

    feat:注册bull任务队列看板

--- backend/src/route.ts
+++ backend/src/route.ts
@@ -1,10 +1,12 @@
 import {Application} from "express";
 import {authRoutes} from '@auth/routes/authRoutes';
+import {serverAdapter} from "@service/queue/base.queue";
 
 const BASE_PATH = '/api/v1'
-export default (app:Application) => {
-    const routes = () => {
-      app.use(BASE_PATH,authRoutes.routes())
-    };
-    routes()
+export default (app: Application) => {
+  const routes = () => {
+    app.use('/queues', serverAdapter.getRouter())
+    app.use(BASE_PATH, authRoutes.routes())
+  };
+  routes()
 }
